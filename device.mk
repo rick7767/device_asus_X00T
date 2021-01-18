0425db9e69f6f3a68226d34f9a935c3a988c797a
Bharat Pawar <bpawar@codeaurora.org>
2021-01-18 13:09:58 +0530

    X00T: Build LED based vibrator Enable LED based vibrator for sdm660 targets.

--- device.mk
+++ device.mk
@@ -388,8 +388,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
     
 # VNDK
 # Update this list with blob function

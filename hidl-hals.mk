762decf3bac67b262cb44ec7418580e8cdfef3f0
Pranav Kapoor <pranavkapoor001@gmail.com>
2021-01-18 12:17:05 +0530

    X00T: Drop livedisplay
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -85,10 +85,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
     
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-    
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service
